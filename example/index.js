016bee2f2f112b4bd2eae0d95a05603a0dbf5213
Max Poshusta <MaxPoshusta@gmail.com>
2021-03-01 07:14:37 -0600

    Initializing the SimplyLazy plugin

--- example/index.js
+++ example/index.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
   // DOM is loaded at this point, so we can call our function to initialize the SimplyLazy plugin
+  SimplyLazy().lazy('img');
 });
