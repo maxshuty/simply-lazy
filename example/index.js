e738501493a030fd894a82cb99d1095d14990906
Max Poshusta <MaxPoshusta@gmail.com>
2021-03-05 05:22:38 -0600

    Wiring up counter

--- example/index.js
+++ example/index.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   // DOM is loaded at this point, so we can call our function to initialize the SimplyLazy plugin
-  SimplyLazy().lazy('img');
+  let counter = 0;
+  const counterEl = document.getElementById('counter');
+  SimplyLazy({
+    onImageLoad: () => {
+      debugger;
+      counter++;
+      counterEl.innerHTML = counter;
+    },
+  }).lazy('img');
 });
