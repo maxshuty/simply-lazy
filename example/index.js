25b4acde73a75e77f5946f6ed158d3da4f08e288
Max Poshusta <MaxPoshusta@gmail.com>
2021-03-07 07:22:16 -0600

    Refactoring the example JS to add the loaded image URL to the card

--- example/index.js
+++ example/index.js
@@ -2,11 +2,25 @@ document.addEventListener('DOMContentLoaded', function () {
   // DOM is loaded at this point, so we can call our function to initialize the SimplyLazy plugin
   let counter = 0;
   const counterEl = document.getElementById('counter');
+  const loadedImagesEl = document.getElementById('loadedImages');
+
+  const updateCounter = () => {
+    counter++;
+    counterEl.innerHTML = counter;
+  };
+
+  const addLoadedImageUrlToCard = (el) => {
+    if (!el) {
+      return;
+    }
+
+    loadedImagesEl.innerHTML += `<div>${counter}. ${el.getAttribute('src')}</div>`;
+  };
+
   SimplyLazy({
-    onImageLoad: () => {
-      // TODO: Max P - pull this logic out into it's own function
-      counter++;
-      counterEl.innerHTML = counter;
+    onImageLoad: (el) => {
+      updateCounter();
+      addLoadedImageUrlToCard(el);
     },
   }).lazy('img');
 });
